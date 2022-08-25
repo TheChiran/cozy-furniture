8f1a87bb2b3ec8034d0cd8d32576d7f46da45fd6
TheChiran <tonmoychiran449@gmail.com>
2022-08-25 22:53:26 +0600

    [Task-> 1. Added required scss files 2. Added some common components]

---
+++ src/components/App/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./../../assets/styles/scss/styles.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Main Component</p>
+    </div>
+  );
+}
+
+export default App;
